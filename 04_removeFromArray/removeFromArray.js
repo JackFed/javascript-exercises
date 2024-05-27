6fbf4e44fed20b687733b8f5610ef95b1a0b32f2
JackFed <jack.fedurek@gmail.com>
2024-05-27 14:16:35 -0400

    Finished basic javascript practices

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    for ( let j = 0; j < theArgs.length; j++) {
+        let arg = theArgs[j];
+        let i = 0;
+        while ( i < arr.length ) {
+            if (arr[i] === theArgs[j]) {
+                arr.splice(i, 1)
+                i--;
+            }
+            i++;
+        }
+
+    }
+    return arr;
 };
+
+removeFromArray([1, 2, 2, 3], 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
