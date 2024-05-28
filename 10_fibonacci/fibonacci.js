fc53f21313261b65b4bc7a9b3f711c5f1f6cd129
JackFed <jack.fedurek@gmail.com>
2024-05-28 15:12:30 -0400

    Finished fib and part of palindrome

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = Number(n);
+    if (num < 0) {
+        return "OOPS";
+    } else if ( num === 0 ){
+        return 0;
+    }
+    let nth = 1;
+    let last = 0;
+    while( num > 1 ){
+        const temp = nth;
+        nth += last;
+        last = temp;
+        num--;
+    }
+    return nth;
 };
 
 // Do not edit below this line
